be1adbf8b90bb406176f7f6a1788e9e62ba96f5e
Adrian T (Big Coder Man) <adriantarantino2006@gmail.com>
2023-08-15 17:21:33 -0400

    Simple audio overlay program for the video

---
+++ main.py
@@ -0,0 +1,20 @@
+# Importing packages required
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+
+# The sample files used (For testing only)
+videoFile: str = "video\\sampleVideo.mp4"
+audioFile: str = "audio\\sampleAudio.mp3"
+
+# Creates clips for both audio and video
+videoFileClip: VideoFileClip = VideoFileClip(videoFile)
+audioFileClip: AudioFileClip = AudioFileClip(audioFile)
+
+# Creates the new clip and writes it to a new file
+newVideoFileClip = videoFileClip.set_audio(audioFileClip)
+newVideoFileClip.write_videofile("output_videos\\sampleOutputVideo.mp4")
+
+# Closes all the files opened
+videoFileClip.close()
+audioFileClip.close()
+newVideoFileClip.close()
