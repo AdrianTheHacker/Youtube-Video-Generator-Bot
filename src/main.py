db77f51e656c1e09108c60b2d02f14708474f313
Adrian T (Big Coder Man) <adriantarantino2006@gmail.com>
2023-08-21 10:53:48 -0400

    Finding API for getting the stories

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import pyttsx3
 
+import requests
+
 from Tools.StockFootageClipMaker import StockFootageClipMaker
 from Tools.TextToSpeechEngine import TextToSpeechEngine
 
+
 def main():
-    storyFilePath = "Media\\Stories\\Story1.txt"
+    storyFilePath = "Media\\Stories\\Story2.txt"
 
     textToSpeechEngine = TextToSpeechEngine()
     textToSpeechEngine.formatTextFile(storyFilePath)
@@ -17,6 +20,22 @@ def main():
 
     stockFootageClipMaker = StockFootageClipMaker("Media\\Stock_Videos\\Video1-MinecraftParkour.mp4", "Media\\Output_Audio\\Story1.mp3", "Media\\Output_videos\\test.mp4")
     stockFootageClipMaker.makeVideoClip()
+
+    # print("Hello, world")
+
+    # url = "https://socialgrep.p.rapidapi.com/search/posts"
+
+    # querystring = {"query":"/r/horrorstories"}
+
+    # headers = {
+	  #   "X-RapidAPI-Key": "Check .ENV",
+	  #   "X-RapidAPI-Host": "Check .ENV"
+    # }
+
+    # response = requests.get(url, headers=headers, params=querystring)
+
+    # print(response.json())
+
 
 if __name__ == '__main__':
     main()
